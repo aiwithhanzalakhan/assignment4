18d3f15f39103392bf580054d6ae3350a5a259d7
50722 <hanzalakhan6226@gmail.com>
2025-04-12 11:19:45 +0500

    solve1

---
+++ assignment_00_to_07/00_intro_python/01_add_two_numbers.py
@@ -0,0 +1,9 @@
+def main():
+    num1:int = int(input("Enter Here First Number: ")) 
+    num2:int = int(input("Enter Here Second Number: ")) 
+
+    print(f"The Sum {num1} + {num2} is: {num1 + num2}")
+
+
+if __name__ == '__main__':
+    main()
